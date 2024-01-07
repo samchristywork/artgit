fe340a511fab473f152c56cc74fa37f70cf8f9c9
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-01-06 20:10:36 -0600

    feat: Add a basic window
    The graphics toolkit I'm going to be using is Fyne. I hope I don't
    regret this decision like I tend to regret most GUI widget toolkit
    decisions.

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	app := app.New()
+	window := app.NewWindow("GUI Application")
+
+	window.ShowAndRun()
+}
